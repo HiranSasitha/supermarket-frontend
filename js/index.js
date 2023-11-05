d2ae75fb2df8a593166f56ffe4efcde239dcfd68
HiranSasi <hiransasitha123@gmail.com>
2023-11-05 23:12:18 +0530

    completed getproduct by category

--- js/index.js
+++ js/index.js
@@ -43,9 +43,49 @@ const getAllCategory = () =>{
         data.map((category)=>{
             const li = document.createElement('li');
             li.innerHTML = category.name;
+            
+            li.addEventListener('click', () => {
+                getProductById(category.id);
+              });
             li.style.cursor = 'pointer';
             li.style.marginBottom = `10px`;
-            categorys.appendChild(li);
+            
+             categorys.appendChild(li);
         });
     }) 
+}
+
+const getProductById = (id) => {
+
+    console.log(id);
+
+    const productsDiv = document.getElementById('products');
+    productsDiv.innerHTML = '';
+    fetch(`http://localhost:8080/categories/${id}/products`).then((response) =>{
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        let products = "";
+
+        data.map((product)=>{
+
+            products+= `<div  class="col-lg-3 col-sm-6 col-12">`;
+
+           products+= `<div class="card mb-4" >`;
+           
+          products+=  `<div class="card-body">`;
+          products+=   ` <h5 class="card-title">${product.name} <br><span class="badge bg-secondary">${product.categoryEntity?.name}</span> </h5>`;
+          products+=   `<p class="card-text">RS. ${product.price}</p>`;
+          products+=   `<a href="#" class="btn btn-primary">View</a>`;
+          products+= `</div>`;
+          products+= `</div>`;
+          products+= `</div>`;
+        
+        });
+
+        productsDiv.innerHTML = products;
+    }).catch((error) => {
+        console.log(error);
+    })
+
 }
